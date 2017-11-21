223f94ea553c902caa49e4f3a3e18d6300e32199
Derek Straka <derek@asterius.io>
2017-11-20 19:26:34 -0500

    Attempt to build all of the native targets for the python recipes
    Signed-off-by: Derek Straka <derek@asterius.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,3 +22,31 @@ echo "TCLIBC=\"${TCLIBC}\"" >> build/conf/local.conf
 
 ./scripts/containerize.sh bitbake -k ${BUILD_TARGETS} || die "failed to build"
 
+# Find all the items that contain a BBCLASSEXTEND for native
+CLASS_EXTENDS_RECIPES=`grep -r BBCLASSEXTEND poky/meta-openembedded/meta-python/ | grep native | cut -d ":" -f1 | xargs -n1 basename | grep \.bb$ | cut -d '_' -f 1`
+CLASS_EXTENDS_INC=`grep -r BBCLASSEXTEND poky/meta-openembedded/meta-python | grep native | cut -d ":" -f1 | xargs -n1 basename | grep \.inc$ | cut -d '.' -f 1`
+
+NATIVE_BUILD_TARGETS=
+
+for recipe in $CLASS_EXTENDS_RECIPES; do
+    NATIVE_BUILD_TARGETS="${NATIVE_BUILD_TARGETS} ${recipe}-native"
+done
+
+for inc in $CLASS_EXTENDS_INC; do
+    # Strip off the leading 'python-'
+    token=`echo $inc | cut -d '-' -f 2-`
+    
+    py2=`find poky/meta-openembedded/meta-python -name python-${token}_\* | wc -l`
+    py3=`find poky/meta-openembedded/meta-python -name python3-${token}_\* | wc -l`
+
+    if [ "$py2" -eq "1" ]; then
+        NATIVE_BUILD_TARGETS="${NATIVE_BUILD_TARGETS} python-${token}-native"
+    fi
+
+    if [ "$py3" -eq "1" ]; then
+        NATIVE_BUILD_TARGETS="${NATIVE_BUILD_TARGETS} python3-${token}-native"
+    fi    
+
+done
+
+./scripts/containerize.sh bitbake -k ${NATIVE_BUILD_TARGETS} || die "failed to build native targets"
